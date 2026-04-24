4a0379ba0ba797a23579f2824afb188dd55330d3
Christian Montoya <chrimonlo@gmail.com>
2026-04-23 19:25:24 -0500

    feat(routes): Define application routes with MainLayout and lazy load PokedexAll component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from '../components/core/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('../pages/pokedex-all/pokedex-all').then((m) => m.PokedexAll),
+      },
+    ],
+  },
+];
