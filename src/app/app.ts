939feffd44f6d0a5ceab8857215a0ce667065677
Christian Montoya <chrimonlo@gmail.com>
2026-04-23 19:25:36 -0500

    refactor(app): Remove PokedexAll component from imports and update app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PokedexAll } from '../pages/pokedex-all/pokedex-all';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PokedexAll],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
